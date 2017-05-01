751262f7fb9bbff3a7012346389673cf21288d42
christinaxiong <christinaxiong@users.noreply.github.com>
2017-05-01 13:20:35 -0700

    Update_Row: Fixed render syntax error and matched names in HTML to key of the corresponding action defined in Controller

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -30,7 +30,6 @@ class DirectorsController < ApplicationController
 
   def update_row
     @director = Director.find(params[:id])
-
     @director.dob = params[":dob"]
     @director.name = params[":name"]
     @director.bio = params[":bio"]
@@ -38,7 +37,7 @@ class DirectorsController < ApplicationController
 
     @director.save
 
-    render("show")
+    render("/directors/show_details.html.erb")
   end
 
   def destroy
